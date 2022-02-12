09f1d9c4d5984292f8aefa4dca23788c2d52fe4e
Björn Fällbom <zcorpio@gmail.com>
2022-02-12 19:52:03 +0100

    creating the application project

---
+++ src/GloboTicket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -0,0 +1,8 @@
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence;
+
+public interface IEventRepository : IAsyncRepository<Event>
+{
+    
+}
