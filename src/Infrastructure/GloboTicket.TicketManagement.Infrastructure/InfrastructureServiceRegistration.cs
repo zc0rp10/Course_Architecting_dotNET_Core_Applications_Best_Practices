fd7dbfd1c911da9e3973fc8acf00d4b010dd140a
Björn Fällbom <bjorn.fallbom@artvise.se>
2022-02-21 11:43:53 +0100

    Added Infrastructure service registrations

---
+++ src/Infrastructure/GloboTicket.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,19 @@
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Models.Mail;
+using GloboTicket.TicketManagement.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboTicket.TicketManagement.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<Email>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailService, EMailService>();
+        
+        return services;
+    }
+}
